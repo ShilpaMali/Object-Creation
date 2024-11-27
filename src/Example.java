b250ea7e87ec7faa307b1c32e2ca6cb795193f4f
Shilpa Mali <malishilpa037@gmail.com>
2024-11-27 13:13:11 +0530

    Object created using classname

---
+++ src/Example.java
@@ -0,0 +1,14 @@
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Syntax: Classname objectname = new Classname();
+		
+		Example example= new Example();
+		System.out.println("Hello Guys");
+		
+
+	}
+
+}
